a29635b2164fdd6f9245027bce2ba4553f398923
Antonio Montero <antoniomonforma99@gmail.com>
2023-01-16 13:24:24 +0100

    list finished

--- lists/lib/main.dart
+++ lists/lib/main.dart
@@ -5,86 +5,12 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-/*
-class Pokemon {
-  int count;
-  String next;
-  Null previous;
-  List<Results> results;
-
-  Pokemon(
-      {this.count,
-      this.next,
-      this.previous,
-      this.results});
-
-  Pokemon.fromJson(Map<String, dynamic> json) {
-    count = json['count'];
-    next = json['next'];
-    previous = json['previous'];
-    if (json['results'] != null) {
-      results = new List<Results>();
-      json['results'].forEach((v) {
-        results.add(new Results.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['count'] = this.count;
-    data['next'] = this.next;
-    data['previous'] = this.previous;
-    if (this.results != null) {
-      data['results'] = this.results.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
-
-class Results {
-  String name;
-  String url;
-
-  Results({this.name, this.url});
-
-  Results.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    url = json['url'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['url'] = this.url;
-    return data;
-  }
-}
-*/
-
-Future<dynamic> fetchPokemon() async {
-  List pokemons = [];
-  final response =
-      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon'));
-
-  if (response.statusCode == 200) {
-    pokemons = (jsonDecode(response.body));
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return pokemons;
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load pokeapi');
-  }
-}
-
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatefulWidget {
-  App({super.key});
+  const App({super.key});
   /*
   late Future<List> futureList;
   */
@@ -94,12 +20,31 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  late Future futurePokemon;
+  var data;
+  fetchPokemon() async {
+    final response = await http.get(
+        Uri.parse("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"));
 
+    if (response.statusCode == 200) {
+      data = (jsonDecode(response.body));
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load pokeapi');
+    }
+  }
+
+/*
+  late Future futurePokemon;
+*/
   @override
   void initState() {
     super.initState();
-    Future pokemons = fetchPokemon();
+    /*
+    Future data = fetchPokemon();
+    */
   }
 
   @override
@@ -119,21 +64,20 @@ class _AppState extends State<App> {
                   child: FutureBuilder(
                 future: fetchPokemon(),
                 builder: (context, snapshot) {
-                  List pokemons = snapshot.data['results'];
-
-                  if (!snapshot.hasData) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
                     return Text('Loading');
                   } else {
                     return ListView.builder(
-                        itemCount: 10,
+                        itemCount: data.length,
                         itemBuilder: (context, index) {
                           return Card(
-                              child: Padding(
-                            padding: const EdgeInsets.all(8.0),
                             child: Column(
-                              children: [Text('' + pokemons[index]['name'])],
+                              children: [
+                                Text('' +
+                                    data['results'][index]['name'].toString()),
+                              ],
                             ),
-                          ));
+                          );
                         });
                   }
                 },
